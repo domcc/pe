91076dd50d82bb6a67f6af8dc3f9162e2e12dc71
Dominic Catherin <dominic.catherin@gmail.com>
2020-03-08 20:23:40 +0800

    Solution to Problem4LargestPalindromeProduct

---
+++ csharp/src/PESolutions/PESolutions/Problems/Problem4LargestPalindromeProduct.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+
+namespace PESolutions.Problems
+{
+    /// <summary>
+    /// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    /// Find the largest palindrome made from the product of two 3-digit numbers.
+    /// </summary>
+    public class Problem4LargestPalindromeProduct
+    {
+        public (int, int, int) Solution(int numberOfDigits)
+        {
+            long largestPrimeFactor = 1;
+            byte [] digits = new byte[6];
+            int largest = 0;
+            int number1 = 0, number2 = 0;
+            int start = (int) Math.Pow(10, numberOfDigits - 1);
+            int end = (int) Math.Pow(10, numberOfDigits);
+            for (int i = start; i < end; i++)
+            {
+                for (int j = start; j < end; j++)
+                {
+                    int value = i * j;
+                    int count = 0;
+                    int value2 = value;
+                    while (value2 > 0)
+                    {
+                        byte digit = (byte)(value2 % 10);
+                        value2 = value2 / 10;
+                        digits[count] = digit;
+                        count++;
+                    }
+
+                    int reverse = 0;
+                    for(int x = 0; x < count; x++)
+                    {
+                        reverse = digits[x] + (reverse * 10);
+                    }
+
+                    if (reverse == value && value > largest)
+                    {
+                        largest = value;
+                        number1 = i;
+                        number2 = j;
+                    }
+                }
+            }
+
+            return (largest, number1, number2);
+        }
+    }
+}
---
+++ csharp/src/tests/PESolutions.UnitTests/Problems/Problem4LargestPalidromeProductTest.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using PESolutions.Problems;
+
+namespace PESolutions.UnitTests.Problems
+{
+    public class Problem4LargestPalindromeProductTest
+    {
+        [Test]
+        public void Problem4LargestPalindromeProductTest1()
+        {
+            Problem4LargestPalindromeProduct problem = new Problem4LargestPalindromeProduct();
+            var solution = problem.Solution(2);
+            Assert.AreEqual(9009, solution.Item1);
+            Assert.AreEqual(91, solution.Item2);
+            Assert.AreEqual(99, solution.Item3);
+        }
+        
+        [Test]
+        public void LargestPrimeFactorTest2()
+        {
+            Problem4LargestPalindromeProduct problem = new Problem4LargestPalindromeProduct();
+            var solution = problem.Solution(3);
+            Assert.AreEqual(906609, solution.Item1);
+            Assert.AreEqual(913, solution.Item2);
+            Assert.AreEqual(993, solution.Item3);
+        }
+    }
+}
