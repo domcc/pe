10de639846926f26eee3964ca94d9850865ea489
Dominic Catherin <dominic.catherin@gmail.com>
2020-03-08 21:15:27 +0800

    Solution to Problem8LargestProductInASeries

---
+++ csharp/src/tests/PESolutions.UnitTests/Problems/Problem8LargestProductInASeriesTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using PESolutions.Problems;
+
+namespace PESolutions.UnitTests.Problems
+{
+    public class Problem8LargestProductInASeriesTest
+    {
+        [Test]
+        public void Problem8LargestProductInASeriesTest1()
+        {
+            Problem8LargestProductInASeries problem = new Problem8LargestProductInASeries();
+            var solution = problem.Solution(4);
+            Assert.AreEqual(5832, solution);
+        }
+        
+        [Test]
+        public void Problem8LargestProductInASeriesTest2()
+        {
+            Problem8LargestProductInASeries problem = new Problem8LargestProductInASeries();
+            var solution = problem.Solution(13);
+            Assert.AreEqual(25164150, solution);
+        }
+    }
+}
